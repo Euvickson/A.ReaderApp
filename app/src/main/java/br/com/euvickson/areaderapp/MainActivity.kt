b84dbb3c26ea67c37f19a9104daafa914522a5f9
Euvickson <victor.eugennio@gmail.com>
2024-03-08 21:01:58 -0300

    -Testing the Firebase.
    Added an user to the Firebase

--- app/src/main/java/br/com/euvickson/areaderapp/MainActivity.kt
+++ app/src/main/java/br/com/euvickson/areaderapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.com.euvickson.areaderapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,18 +12,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.euvickson.areaderapp.ui.theme.AReaderAppTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AReaderAppTheme {
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "Jeo"
+                user["lastName"] = "James"
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+
+                    db.collection("users").add(user).addOnSuccessListener {
+                        Log.d("FB", "onCreate: ${it.id}")
+                    }.addOnFailureListener {
+                        Log.d("FB", "onCreate: ${it}")
+                    }
                 }
             }
         }
