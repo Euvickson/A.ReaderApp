aa60b9d7ebb5b3c6dc35fcbdf01ea287620172af
Euvickson <victor.eugennio@gmail.com>
2024-03-21 12:28:33 -0300

    - Adding the user to the firebase database, instead of only adding to the authentication

--- app/src/main/java/br/com/euvickson/areaderapp/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/br/com/euvickson/areaderapp/screens/login/LoginScreenViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
@@ -23,7 +24,6 @@ class LoginScreenViewModel: ViewModel() {
                 .addOnCompleteListener {task ->
                     if (task.isSuccessful) {
                         Log.d("FB", "signInWithEmailAndPassword: Yayayay! ${task.result.toString()}")
-                       //TODO: take them home
                         home()
                     } else{
                         Log.d("FB", "signInWithEmailAndPassword: ${task.result.toString()}")
@@ -43,6 +43,8 @@ class LoginScreenViewModel: ViewModel() {
             _loading.value = true
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {task ->
                 if (task.isSuccessful) {
+                    val displayName = task.result.user?.email?.split('@')?.get(0)
+                    createUser(displayName)
                     home()
                 } else {
                     Log.d("FB", "createUserWithEmailAndPassword: ${task.result.toString()}")
@@ -51,6 +53,15 @@ class LoginScreenViewModel: ViewModel() {
             }
         }
 
+    }
+
+    private fun createUser(displayName: String?) {
+        val userId = auth.currentUser?.uid
+        val user = mutableMapOf<String, Any>()
+        user["user_id"] = userId.toString()
+        user["display_name"] = displayName.toString()
+
+        FirebaseFirestore.getInstance().collection("users").add(user)
     }
 
 }
