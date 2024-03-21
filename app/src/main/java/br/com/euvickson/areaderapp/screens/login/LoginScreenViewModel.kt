d185db60dc69be2735bf135e2a1c409050e9ed50
Euvickson <victor.eugennio@gmail.com>
2024-03-21 12:47:21 -0300

    - Creating a model to use in the Firebase database, so it has more info and it's more automatic, instead of doing manually

--- app/src/main/java/br/com/euvickson/areaderapp/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/br/com/euvickson/areaderapp/screens/login/LoginScreenViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.euvickson.areaderapp.model.MUser
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -57,9 +58,14 @@ class LoginScreenViewModel: ViewModel() {
 
     private fun createUser(displayName: String?) {
         val userId = auth.currentUser?.uid
-        val user = mutableMapOf<String, Any>()
-        user["user_id"] = userId.toString()
-        user["display_name"] = displayName.toString()
+        val user = MUser(
+            userId = userId.toString(),
+            displayName = displayName.toString(),
+            avatarUrl = "",
+            quote = "Life is Great",
+            profession = "Android Developer",
+            id = null
+        ).toMap()
 
         FirebaseFirestore.getInstance().collection("users").add(user)
     }
