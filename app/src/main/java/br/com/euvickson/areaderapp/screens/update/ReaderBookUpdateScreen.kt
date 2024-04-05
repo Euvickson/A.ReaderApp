122cdecc97a939fa7fb8c059a2789375079cdc1b
Euvickson <victor.eugennio@gmail.com>
2024-04-04 21:07:05 -0300

    - Added the Start and Finished Reading TextButtons
    - Code Refactoring

--- app/src/main/java/br/com/euvickson/areaderapp/screens/update/ReaderBookUpdateScreen.kt
+++ app/src/main/java/br/com/euvickson/areaderapp/screens/update/ReaderBookUpdateScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
@@ -33,7 +34,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -50,9 +55,13 @@ import coil.compose.rememberAsyncImagePainter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BookUpdateScreen(navController: NavHostController, bookItemId: String, viewModel: HomeScreenViewModel = hiltViewModel()) {
+fun BookUpdateScreen(
+    navController: NavHostController,
+    bookItemId: String,
+    viewModel: HomeScreenViewModel = hiltViewModel()
+) {
 
-    Scaffold (
+    Scaffold(
         topBar = {
             ReaderAppBar(
                 title = "Update Book",
@@ -63,24 +72,37 @@ fun BookUpdateScreen(navController: NavHostController, bookItemId: String, viewM
                 navController.popBackStack()
             }
         }
-    ){
-        val bookInfo = produceState<DataOrException<List<MBook>, Boolean, Exception>>(initialValue = DataOrException(data = emptyList(), true, Exception(""))) {
+    ) {
+        val bookInfo = produceState<DataOrException<List<MBook>, Boolean, Exception>>(
+            initialValue = DataOrException(
+                data = emptyList(),
+                true,
+                Exception("")
+            )
+        ) {
             value = viewModel.data.value
         }.value
 
-        Surface (modifier = Modifier
-            .padding(it)
-            .fillMaxSize()
-            .padding(3.dp)){
+        Surface(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .padding(3.dp)
+        ) {
 
             Column(
                 modifier = Modifier.padding(top = 3.dp),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){
+            ) {
 
                 if (bookInfo.loading == true) {
-                    LinearProgressIndicator(modifier = Modifier.padding(vertical = 4.dp, horizontal = 12.dp))
+                    LinearProgressIndicator(
+                        modifier = Modifier.padding(
+                            vertical = 4.dp,
+                            horizontal = 12.dp
+                        )
+                    )
                     bookInfo.loading = false
                 } else {
                     Surface(
@@ -89,22 +111,23 @@ fun BookUpdateScreen(navController: NavHostController, bookItemId: String, viewM
                             .fillMaxWidth(),
                         shape = CircleShape,
                         shadowElevation = 4.dp
-                    ){
+                    ) {
                         ShowBookUpdate(bookInfo = viewModel.data.value, bookItemId = bookItemId)
                     }
 
-                    ShowSimpleForm(book = viewModel.data.value.data?.first {mBook ->
+                    ShowSimpleForm(book = viewModel.data.value.data?.first { mBook ->
                         mBook.googleBookId == bookItemId
                     }!!, navController = navController)
 
                 }
+
 
             }
 
         }
 
     }
-    
+
 }
 
 @Composable
@@ -112,8 +135,59 @@ fun ShowSimpleForm(book: MBook, navController: NavHostController) {
 
     val notesText = remember { mutableStateOf("") }
 
+    val isStartedReading = remember {
+        mutableStateOf(false)
+    }
+
+    val isFinishedReading = remember {
+        mutableStateOf(false)
+    }
+
     SimpleForm(defaultValue = book.notes.toString().ifEmpty { "No Thoughts available." }) { note ->
         notesText.value = note
+    }
+
+    Row(
+        modifier = Modifier.padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+
+        TextButton(
+            onClick = { isStartedReading.value = true },
+            enabled = book.startedReading == null
+        ) {
+            if (book.startedReading == null) {
+                if (!isStartedReading.value) {
+                    Text(text = "Start Reading")
+                } else {
+                    Text(
+                        text = "Started Reading!",
+                        modifier = Modifier.alpha(0.6f),
+                        color = Color.Red
+                    )
+                }
+            } else {
+                Text(text = "Started on: ${book.startedReading}") //todo: Format Date
+            }
+        }
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        TextButton(onClick = { isFinishedReading.value = true }, enabled = book.finishedReading == null) {
+            if (book.finishedReading == null) {
+                if (!isFinishedReading.value) {
+                    Text(text = "Mark as Read")
+                } else {
+                    Text(text = "Finished Reading!",
+                        modifier = Modifier.alpha(0.6f),
+                        color = Color.Red)
+                }
+            } else {
+                Text(text = "Finished on: ${book.finishedReading}") //todo: Format Date
+            }
+
+        }
     }
 }
 
@@ -128,7 +202,7 @@ fun SimpleForm(
     Column {
         val textFieldValue = rememberSaveable { mutableStateOf(defaultValue) }
         val keyboardController = LocalSoftwareKeyboardController.current
-        val valid = remember (textFieldValue.value) { textFieldValue.value.trim().isNotEmpty() }
+        val valid = remember(textFieldValue.value) { textFieldValue.value.trim().isNotEmpty() }
 
         InputField(
             modifier = Modifier
@@ -156,12 +230,11 @@ fun SimpleForm(
 @Composable
 fun ShowBookUpdate(bookInfo: DataOrException<List<MBook>, Boolean, Exception>, bookItemId: String) {
     Row {
-        Spacer(modifier = Modifier.width(43.dp))
 
         if (bookInfo.data != null) {
-            Column (modifier = Modifier.padding(4.dp), verticalArrangement = Arrangement.Center){
+            Column(modifier = Modifier.background(MaterialTheme.colorScheme.onSecondary).padding(4.dp), verticalArrangement = Arrangement.Center) {
 
-                CardListItem(book = bookInfo.data!!.first {mBook ->
+                CardListItem(book = bookInfo.data!!.first { mBook ->
                     mBook.googleBookId == bookItemId
                 }, onPressDetails = {})
 
@@ -173,20 +246,22 @@ fun ShowBookUpdate(bookInfo: DataOrException<List<MBook>, Boolean, Exception>, b
 
 @Composable
 fun CardListItem(book: MBook, onPressDetails: () -> Unit) {
-    Card(modifier = Modifier
-        .padding(
-            start = 4.dp,
-            end = 4.dp,
-            top = 4.dp,
-            bottom = 8.dp
-        )
-        .fillMaxWidth()
-        .clip(RoundedCornerShape(20.dp))
-        .clickable { },
+    Card(
+        modifier = Modifier
+            .padding(
+                start = 4.dp,
+                end = 4.dp,
+                top = 4.dp,
+                bottom = 8.dp
+            )
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(60.dp))
+            .clickable { },
         elevation = CardDefaults.cardElevation(8.dp),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.onSecondary)) {
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.onSecondary)
+    ) {
 
-        Row (horizontalArrangement = Arrangement.Start) {
+        Row(horizontalArrangement = Arrangement.Start) {
 
             Image(
                 painter = rememberAsyncImagePainter(model = book.photoUrl.toString()),
@@ -205,14 +280,14 @@ fun CardListItem(book: MBook, onPressDetails: () -> Unit) {
                     )
             )
 
-            Column {
+            Column (Modifier.fillMaxWidth()) {
                 Text(
                     text = book.title.toString(),
                     style = MaterialTheme.typography.bodySmall,
                     fontSize = 18.sp,
                     modifier = Modifier
                         .padding(start = 8.dp, end = 8.dp)
-                        .width(120.dp),
+                        .fillMaxWidth(),
                     fontWeight = FontWeight.SemiBold,
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
